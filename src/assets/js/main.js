e438de32a61fc1ee5a26b5f7cd3ad0cd7dfea3e4
daveyedgar <62836024+daveyedgar@users.noreply.github.com>
2023-01-08 14:41:00 -0500

    formatted numbers

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,5 @@
 
-// validate user form inputs
-// set numbers to comma 1,000
+
 // extra payment field
 // down payment field?
 // tax, insurance, hoa, mortgage ins,
@@ -8,6 +7,7 @@
 
 document.getElementById("monthly").addEventListener("change", getYearly);
 document.getElementById("yearly").addEventListener("change", getMonthly);
+document.getElementById("loanAmount").addEventListener("change", formatAmount);
 document.getElementById("btnSubmit").addEventListener("click", getValues);
 
 function getMonthly(){
@@ -22,6 +22,14 @@ function getYearly(){
   let yearly = document.getElementById("yearly");
   monthly.focus();
   yearly.value = (monthly.value / 12).toFixed(2);
+}
+
+function formatAmount(){
+  let fSpan = document.getElementById("fAmount");
+  fSpan.focus();
+  let amountInput = document.getElementById("loanAmount");
+  let fAmount = parseFloat(amountInput.value).toLocaleString('en-US');
+  fSpan.innerHTML = "($" + fAmount + ")";
 }
 
 function getValues() {
@@ -104,14 +112,22 @@ function displayResults(results) {
     ttlCost += +monthlyPayment;
     totalCost = ttlCost.toFixed(2);
 
+    // format numbers
+    monPayment = parseFloat(monthlyPayment).toLocaleString('en-US');
+    princPayment = parseFloat(principalPayment).toLocaleString('en-US');
+    remBalance = parseFloat(remainingBalance).toLocaleString('en-US');
+    intPayment = parseFloat(interestPayment).toLocaleString('en-US');
+    accruedInt = parseFloat(accruedInterest).toLocaleString('en-US');
+
+
     templateRows = templateRows + 
     `<tr>
       <td>${month}</td>
-      <td>${monthlyPayment}</td>
-      <td>${principalPayment}</td>
-      <td>${interestPayment}</td>
-      <td>${accruedInterest}</td>
-      <td>${remainingBalance}</td>
+      <td>${monPayment}</td>
+      <td>${princPayment}</td>
+      <td>${intPayment}</td>
+      <td>${accruedInt}</td>
+      <td>${remBalance}</td>
     </tr>`;
   }
 
@@ -119,17 +135,21 @@ function displayResults(results) {
 
   // this is for the header, not the table
   let monthlyPaymentTag = document.getElementById("monthlyPayment");
-  monthlyPaymentTag.innerHTML = results[0].monthlyPayment.toFixed(2);
+  let mPayment = results[0].monthlyPayment.toFixed(2);
+  monthlyPaymentTag.innerHTML = parseFloat(mPayment).toLocaleString('en-US');
 
   let totalPrincipalTag = document.getElementById("totalPrincipal");
   let loanAmount = document.getElementById("loanAmount").value;
-  totalPrincipalTag.innerHTML = loanAmount;
+  let lAmount = parseFloat(loanAmount).toLocaleString('en-US');
+  totalPrincipalTag.innerHTML = lAmount;
 
   let totalInterestTag = document.getElementById("totalInterest");
-  totalInterestTag.innerHTML = totalInterest;
+  let tInterest = parseFloat(totalInterest).toLocaleString('en-US');
+  totalInterestTag.innerHTML = tInterest;
 
   let totalCostTag = document.getElementById("totalCost");
-  totalCostTag.innerHTML = totalCost;
+  let tCost = parseFloat(totalCost).toLocaleString('en-US');
+  totalCostTag.innerHTML = tCost;
 
   // see the code link
   let codeLink = document.getElementById("codeLink");
